4bb4301b9e586d5dbbd68f6efd4257d7368a1c19
Onore849 <rion3376@gmail.com>
2020-08-28 19:51:38 +0900

    Initial Commit

---
+++ collecionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collecionView
+//
+//  Created by 野澤拓己 on 2020/08/28.
+//  Copyright © 2020 Takumi Nozawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
