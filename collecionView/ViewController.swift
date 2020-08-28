1e36d37a8d6d136814267dcfdcca7401ef745b03
Onore849 <rion3376@gmail.com>
2020-08-28 20:51:45 +0900

    create collectionView

--- collecionView/ViewController.swift
+++ collecionView/ViewController.swift
@@ -8,13 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Model {
+    
+    let text: String
+    let imageName: String
+    
+    init(text: String, imageName: String) {
+        
+        self.text = text
+        self.imageName = imageName
+        
+    }
+    
+}
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var table: UITableView!
+    
+    var models = [Model]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        models.append(Model(text: "First", imageName: "bike1"))
+        models.append(Model(text: "Second", imageName: "bike2"))
+        models.append(Model(text: "Third", imageName: "bike3"))
+        models.append(Model(text: "Demo", imageName: "bike4"))
+        
+        models.append(Model(text: "First", imageName: "bike1"))
+        models.append(Model(text: "Second", imageName: "bike2"))
+        models.append(Model(text: "Third", imageName: "bike3"))
+        models.append(Model(text: "Demo", imageName: "bike4"))
+        
+        models.append(Model(text: "First", imageName: "bike1"))
+        models.append(Model(text: "Second", imageName: "bike2"))
+        models.append(Model(text: "Third", imageName: "bike3"))
+        models.append(Model(text: "Demo", imageName: "bike4"))
+        
+        
+        table.register(CollectionTableViewCell.nib(), forCellReuseIdentifier: CollectionTableViewCell.identifier)
+        
+        table.delegate = self
+        table.dataSource = self
+        
     }
+    
+    // Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = table.dequeueReusableCell(withIdentifier: CollectionTableViewCell.identifier, for: indexPath) as! CollectionTableViewCell
+        
+        cell.configure(with: models)
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 250.0
+        
+    }
+    
 
 
 }
+
 
